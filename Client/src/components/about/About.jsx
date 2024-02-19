6d191ade6339d625782656f23edac35d6a3f66a5
madifrontend <madifrontend@gmail.com>
2024-02-19 18:15:18 +0330

    [update] add comment comp to about page

--- Client/src/components/about/About.jsx
+++ Client/src/components/about/About.jsx
@@ -7,35 +7,17 @@ import { FaRegComment } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import { format } from "date-fns";
 import { UserContext } from "../../userContext";
+import Comments from "../comment/comment";
 
 const About = () => {
   const [postInfo, setPostInfo] = useState(null);
-  const [newComment, setNewComment] = useState(null);
   const { id } = useParams();
   const { userInfo } = useContext(UserContext);
 
-  const addComment = async (ev) => {
-    ev.preventDefault();
-    const response = await fetch(`http://localhost:3001/comment/${id}`, {
-      method: "POST",
-      body: JSON.stringify(newComment),
-      headers: { "content-Type": "application/json" },
-    });
-    if (response.status === 200) {
-      alert("your comment sent!");
-    } else {
-      alert("we cannot add your comment!");
-    }
-    console.log(id);
-  };
-  console.log(newComment);
   useEffect(() => {
     fetch(`http://localhost:3001/post/${id}`).then((res) =>
       res.json().then((posts) => setPostInfo(posts))
     );
-    // fetch(`http://localhost:3001/comment/${id}`).then((res) =>
-    //   res.json().then((posts) => setPostInfo(posts))
-    // );
   }, []);
 
   return (
@@ -105,11 +87,13 @@ const About = () => {
               <span className="text-[#929191] text-[25px]">,,,,,</span>
             </div>
           </div>
-
           <div className="flex gap-5 justify-center items-center mt-10">
             <FaInstagram size={22} color="" />
             <FiFacebook size={22} color="" />
             <FiTwitter size={22} color="" />
+          </div>
+          <div className="mt-20">
+            <Comments postInfo={postInfo} userInfo={userInfo} />
           </div>
         </div>
       ) : (
@@ -117,17 +101,6 @@ const About = () => {
           <img src={spin} alt="spin Logo" className="w-[80px]" />
         </div>
       )}
-      <div>
-        <form onSubmit={addComment}>
-          <input
-            type="text"
-            onChange={(ev) => setNewComment(ev.target.value)}
-            placeholder="newComment"
-            name="newComment"
-          />
-          <button type="submit">add comment</button>
-        </form>
-      </div>
     </div>
   );
 };
